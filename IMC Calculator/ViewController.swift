4721ef4e6e6b164d98f6e6a87c012df7f66e8fe4
Yan Oliveira <oliveira.yan02@gmail.com>
2022-09-01 13:15:03 -0300

    Initial Commit

---
+++ IMC Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC Calculator
+//
+//  Created by Yan Oliveira on 01/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
