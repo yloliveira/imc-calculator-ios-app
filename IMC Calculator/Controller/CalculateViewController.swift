9f43967d5f811bbd882ab0e9a536535186bcfdc1
Yan Oliveira <oliveira.yan02@gmail.com>
2022-09-01 15:54:37 -0300

    feat: add layout component connections

--- IMC Calculator/Controller/CalculateViewController.swift
+++ IMC Calculator/Controller/CalculateViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class CalculateViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBAction func heightValueChanged(_ sender: UISlider) {
+        print("height value: \(sender.value)")
+    }
+    
+    @IBAction func weightValueChanged(_ sender: UISlider) {
+        print("weight value: \(sender.value)")
+    }
+    
+    @IBAction func CalculateButtonPressed(_ sender: UIButton) {
+        print("calculate pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
