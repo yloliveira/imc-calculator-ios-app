c2b72ac1a4ef81656fd07217cb08721d9ba9ef9d
Yan Oliveira <oliveira.yan02@gmail.com>
2022-09-02 09:49:48 -0300

    feat: add initial value and calculate bmi when button is pressed

--- IMC Calculator/Controller/CalculateViewController.swift
+++ IMC Calculator/Controller/CalculateViewController.swift
@@ -14,22 +14,35 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var weightSlider: UISlider!
     
-    @IBAction func heightValueChanged(_ sender: UISlider) {
-        print("height value: \(sender.value)")
-    }
-    
-    @IBAction func weightValueChanged(_ sender: UISlider) {
-        print("weight value: \(sender.value)")
-    }
-    
-    @IBAction func CalculateButtonPressed(_ sender: UIButton) {
-        print("calculate pressed")
-    }
+    let HEIGHT_UNIT = "m"
+    let WEIGHT_UNIT = "kg"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        heightLabel.text = "\(height)\(HEIGHT_UNIT)"
+        weightLabel.text = "\(weight)\(WEIGHT_UNIT)"
     }
-
+    
+    @IBAction func heightValueChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)\(HEIGHT_UNIT)"
+    }
+    
+    @IBAction func weightValueChanged(_ sender: UISlider) {
+        let weight = String(format: "%.1f", sender.value)
+        weightLabel.text = "\(weight)\(WEIGHT_UNIT)"
+    }
+    
+    @IBAction func CalculateButtonPressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        print("calculate pressed: \(String(format: "%.2f", bmi))")
+    }
+    
 }
 
